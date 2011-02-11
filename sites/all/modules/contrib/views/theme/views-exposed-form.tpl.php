1249cb08d4f4dbf6bdb7d3c4bc482c99ad9bc9b9
kenorb <kenorb@gmail.com>
2011-02-11 18:07:58 +0000

    #63: upgrade to latest dev, because of bug: http://drupal.org/node/943194
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@35 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/modules/contrib/views/theme/views-exposed-form.tpl.php
+++ sites/all/modules/contrib/views/theme/views-exposed-form.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: views-exposed-form.tpl.php,v 1.4.2.3 2010/03/10 19:57:13 merlinofchaos Exp $
+// $Id: views-exposed-form.tpl.php,v 1.4.2.5 2010/08/13 21:45:28 merlinofchaos Exp $
 /**
  * @file views-exposed-form.tpl.php
  *
@@ -30,7 +30,7 @@
 <div class="views-exposed-form">
   <div class="views-exposed-widgets clear-block">
     <?php foreach($widgets as $id => $widget): ?>
-      <div class="views-exposed-widget">
+      <div class="views-exposed-widget views-widget-<?php print $id ?>">
         <?php if (!empty($widget->label)): ?>
           <label for="<?php print $widget->id; ?>">
             <?php print $widget->label; ?>
@@ -64,13 +64,13 @@
         <?php print $offset; ?>
       </div>
     <?php endif; ?>
+    <div class="views-exposed-widget views-submit-button">
+      <?php print $button ?>
+    </div>
     <?php if (!empty($reset_button)): ?>
-      <div class="views-exposed-widget">
+      <div class="views-exposed-widget views-reset-button">
         <?php print $reset_button; ?>
       </div>
     <?php endif; ?>
-    <div class="views-exposed-widget">
-      <?php print $button ?>
-    </div>
   </div>
 </div>
