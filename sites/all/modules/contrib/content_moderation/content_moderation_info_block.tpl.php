296532eff99f9d188beab5e84b92fdc1943c86ba
kenorb <kenorb@gmail.com>
2011-03-10 22:20:12 +0000

    #150: added suitable module for moderation
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@69 2260b2a0-9455-4d21-9559-270ed330a898

---
+++ sites/all/modules/contrib/content_moderation/content_moderation_info_block.tpl.php
@@ -0,0 +1,75 @@
+<?php
+// $Id: content_moderation_info_block.tpl.php,v 1.6.2.23 2010/09/29 15:16:03 eugenmayer Exp $
+
+# Copyright (c) 2010 Impressive.media
+# Author: Eugen Mayer
+/*
+ * Usable values
+ * $node : currently viewed node
+ * $state: workflow state of the currently viewed node revision
+ * $live: live version of the node
+ * $live_link: revision and link to the live node
+ * $links: actions on the different revisions (compare_with_live,compare,delete_revision, edit_revision, edit_live)
+ */
+
+
+$current_user = user_load($node->revision_uid);
+
+$edit_icon = '<span class="tinyicon t_editicon"></span>';
+$rev_icon = '<span class="tinyicon t_revisionicon"></span>';
+$edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
+$delete_icon = '<span class="tinyicon t_deleteicon"></span>';
+$view_icon = '<span class="tinyicon t_viewicon"></span>';
+
+
+/* actions */
+if(_content_moderation_statechange_allowed($node->vid)) {
+  $edit_state = l($edit_state_icon,$links['edit_state'],array('html' => true, 'attributes' => array( 'title' => t('Edit the state of this revision.') ) ));
+}
+
+if($live != NULL) {
+  $view_live_link = l($view_icon,$links['live_view'],array('html' => true, 'attributes' => array( 'title' => t('View live revision') ) ));
+
+}
+$check_revisions = _content_moderation_get_latest_revisions($node->nid, 2);
+$rev_url = _content_moderation_determine_revision_url($check_revisions, $node->nid, $node->vid, FALSE);
+$view_current_link = l($view_icon, $rev_url,array('html' => true, 'attributes' => array( 'title' => t('View this revision.') ) ));
+$current_rev_link = l($node->vid, $rev_url,array('html' => true, 'attributes' => array( 'title' => t('View this revision.') ) ));
+
+if(module_exists('diff')){
+  $compare_live = l($rev_icon,$links['compare_with_live'],array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the live revision.') ) ));
+  $compare = l($rev_icon,$links['compare'],array('html' => true, 'attributes' => array( 'title' => t('List all revisions.') ) ));
+}
+// TODO: first see how revision deleting, esp. the live ones, should be handled
+//$delete_current = l($delete_icon,$links['delete_revision'],array('html' => true, 'attributes' => array( 'title' => t('Delete this revision') ) ));
+
+?>
+<div id="content_moderation">
+
+  <h4><?php print t('Live')?></h4>
+  <?php if($live != NULL) {
+    $live_user = user_load($live->revision_uid);
+    ?>
+    <div class="info live_info"><?php print $live->vid?>: <?=$live_link.$view_live_link.$compare?><br/>
+    <span class="details">&raquo; <?php print format_date($live->revision_timestamp, 'small')?> (<?php print $live_user->name?>)</span>
+    </div>
+  <?php }  else { ?>
+    <div class="info live_info"><?php print t('nothing approved yet');?></div>
+  <?php } ?>
+
+
+  <?php if($live == NULL || $node->vid != $live->vid) {?>
+  <h4><?php print t('Viewing')?></h4>
+  <div class="info"><label><?php print $current_rev_link?>:</label> (<?php print $state?>) <?php print $view_current_link.$edit_state.$compare_live.$delete_current?><br/>
+    <span class="details">&raquo; <?php print format_date($node->revision_timestamp, 'small')?> (<?php print $current_user->name?>)</span>
+    <span class="details state">&raquo; Status: <?php print t($state)?></span>
+  </div>
+  <?php }?>
+
+  <?php if($revisions_list != "") { ?>
+  <h4><?php print t('Pending')?></h4>
+     <div class="info">
+        <?php echo $revisions_list; ?>
+     </div>
+   <?php } ?>
+</div>
---
+++ sites/all/modules/contrib/content_moderation/content_moderation_revisions_list.tpl.php
@@ -0,0 +1,58 @@
+<?php
+// $Id: content_moderation_revisions_list.tpl.php,v 1.2.2.23 2010/09/29 15:16:03 eugenmayer Exp $
+
+# Copyright (c) 2010 Impressive.media
+# Author: Eugen Mayer
+/*
+ * Usable values
+ * $node : currently viewed node
+ * $state: workflow state of the currently viewed node revision
+ * $live: live version of the node
+ * $live_link: revision and link to the live node
+ * $links: actions on the different revisions (compare_with_live,compare,delete_revision, edit_revision, edit_live)
+ */
+
+if(!is_array($revisions) || count($revisions) == 0) {
+  return "";
+}
+?>
+<div class='revision_list'>
+<?php
+$edit_icon = '<span class="tinyicon t_editicon"></span>';
+$rev_icon = '<span class="tinyicon t_revisionicon"></span>';
+$view_icon = '<span class="tinyicon t_viewicon"></span>';
+$edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
+
+foreach($revisions as $rev) {
+  $edit_state_link = '';
+  $compare_live = '';
+  $compare = '';
+  $check_revisions = _content_moderation_get_latest_revisions($rev->nid, 2);
+  $rev_url = _content_moderation_determine_revision_url($check_revisions, $rev->nid, $rev->vid, FALSE);
+  $view_link = l($view_icon, $rev_url,array('html' => true, 'attributes' => array( 'title' => t('View revision @rev.',array('@rev' => $rev->vid) ))));
+  $revision_link = l($rev->vid, $rev_url,array('html' => true, 'attributes' => array( 'title' => t('View revision @rev.',array('@rev' => $rev->vid) ))));
+
+  if(_content_moderation_statechange_allowed($rev->vid) !== false) {
+    $edit_state_link = l($edit_state_icon,_content_moderation_change_state_link($rev->vid,$rev->nid),array('html' => true, 'attributes' => array( 'title' => t('Change state of revision @rev.',array('@rev' => $rev->vid) ))));
+  }
+  if(module_exists('diff')){
+    $live = _content_moderation_live_revision($node->nid);
+    if($live != NULL) {
+      $compare_live = l($rev_icon,"node/{$rev->nid}/revisions/view/{$live->vid}/{$rev->vid}",array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the current live revision.') ) ));
+    }
+    $compare = l($rev_icon,$links['compare'],array('html' => true, 'attributes' => array( 'title' => t('List all revisions.') ) ));
+  }
+  $compare = 'compare';
+
+  if($rev->state != NULL) {
+    $state = ucfirst(t($rev->state));
+  }
+  else {
+    $state  = ucfirst(t('none'));
+  }
+?>
+<span class="revision"><?php print $revision_link?>: (<?php print $state?>) <?php print $view_link?><?php print $edit_state_link?><?php print $compare_live?></span>
+<?php
+}
+?>
+</div>
