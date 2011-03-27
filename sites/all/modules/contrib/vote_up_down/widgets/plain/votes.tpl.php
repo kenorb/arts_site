da322775cd7e69b1f2983e19705df4c45e890590
kenorb <kenorb@gmail.com>
2011-03-27 14:38:43 +0000

    #170: vote_up_down module
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@77 2260b2a0-9455-4d21-9559-270ed330a898

---
+++ sites/all/modules/contrib/vote_up_down/widgets/plain/votes.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id: votes.tpl.php,v 1.2 2010/05/31 03:59:40 marvil07 Exp $
+/**
+ * @file
+ * votes.tpl.php
+ *
+ * Plain widget votes display for Vote Up/Down
+ */
+?>
+<span id="<?php print $id; ?>" class="total-votes-plain"><span class="<?php print $class; ?> total"><?php print $unsigned_points; ?> <?php print $vote_label; ?></span></span>
