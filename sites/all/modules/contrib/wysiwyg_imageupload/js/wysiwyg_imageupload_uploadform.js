fe4312108bc0cf826d498afb5d09f4c6a40d6c22
akierzkowski <adrian.kierzkowski@gmail.com>
2011-10-31 13:43:49 +0000

    #278 Bug: Invalid argument supplied for foreach() in _wysiwyg_imageupload_save() Updating Wysiwyg_imageupload module to 6.x-2.x-dev from 2011-May-21
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@140 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/modules/contrib/wysiwyg_imageupload/js/wysiwyg_imageupload_uploadform.js
+++ sites/all/modules/contrib/wysiwyg_imageupload/js/wysiwyg_imageupload_uploadform.js
@@ -5,7 +5,7 @@ Drupal.behaviors.wysiwyg_imageupload_uploadform = function () {
   $('#edit-wysiwyg-imageupload-file').bind('change', function() {
       $('.dialog-page-content *').hide();
       $('.dialog-page-content').addClass('uploading');
-      $('form').submit();
+      $(this).parents('form').submit();
     }
   );
 }
