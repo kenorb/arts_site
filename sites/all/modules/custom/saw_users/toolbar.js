a93fdf82c04f324c732c30e372c6976387d1a0b2
akierzkowski <adrian.kierzkowski@gmail.com>
2011-11-04 10:58:27 +0000

    #216 Toolbar - Sticky menu at the top Now toolbar is stick to the top
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@173 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/modules/custom/saw_users/toolbar.js
+++ sites/all/modules/custom/saw_users/toolbar.js
@@ -1,14 +1,23 @@
 
+  var initialToolbarPositionTop;
+
   var onUpdateToolbar = function ()
   {
+    if (!initialToolbarPositionTop)
+      initialToolbarPositionTop = Math.ceil ($('#toolbar-area').offset ().top + 1);
+    
     var windowScrollTop = $(window).scrollTop ();
     
-    if (windowScrollTop > 94)
+    if (windowScrollTop > $('#toolbar-area').offset ().top)
+    {
       $('#quickbar').addClass ('stick');
+      $('#quickbar').css ('top', 'auto');
+    }
     else
+    {
       $('#quickbar').removeClass ('stick');
+      $('#quickbar').css ('top', initialToolbarPositionTop + 'px'); 
+    }
   };
 
   $(window).scroll (onUpdateToolbar);
-  
-  
