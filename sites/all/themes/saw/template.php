5d53e2dfd9da37acb69b615e244af621ad908274
akierzkowski <adrian.kierzkowski@gmail.com>
2011-11-07 10:34:15 +0000

    #291 Forum: All forum subtopics should be hidden on main page Resolved
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@181 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/themes/saw/template.php
+++ sites/all/themes/saw/template.php
@@ -6,4 +6,36 @@ function student_art_preprocess_page(&$vars) {
 function menu_tertiary_links() {
   return menu_navigation_links(variable_get('menu_primary_links_source', 'primary-links'), 2);
 }
+
+/**
+ * Forum hook. Provides:
+ * - Removes unnecesary subforums (#291)
+ * @param array $variables
+ */
+function saw_preprocess_forum_list (&$variables) {
+	
+	/*
+	 * #291: Removes unnecesary subforums. 
+	 */
+	
+	if (!count ($variables ['forums']))
+	// No forums - nothing to do
+		return;
+
+	$keys			= array_keys ($variables ['forums']);
+	$numKeys	= count ($keys);
+	
+	for ($i = 0; $i < count ($keys); $i++)
+	// Traversing all the forums
+		if ($variables ['forums'] [$keys [$i]] -> is_container)
+		// This forum is a container
+			for ($j = 0; $j < count ($keys); $j++)
+			// Traversing all the forums again
+				foreach ($variables ['forums'] [$keys [$j]] -> parents as $parent)
+				// Traversing this forum parents
+					if ($parent == $keys [$i])
+					// If this forum has visible parent container then it is destroyed
+						unset ($variables ['forums'] [$keys [$j]]);
+}
+
 ?>
