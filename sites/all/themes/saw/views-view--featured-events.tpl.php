ce4ba94b5c9701ed8539e8e6009abf81f4ac4a65
akierzkowski <adrian.kierzkowski@gmail.com>
2011-11-09 08:47:21 +0000

    #263: Events: Calendar: List Featured Events at the bottom of Calendar (Find, In, Create An Event) Changed template logic. Added some CSS styles. Changed featured events view.
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@190 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/themes/saw/views-view--featured-events.tpl.php
+++ sites/all/themes/saw/views-view--featured-events.tpl.php
@@ -19,30 +19,27 @@
   	{
 	  	function featured_events_get_time ($row)
 	  	{
-	  		preg_match_all ('/([a-zA-Z]+), ([0-9]+)\/([0-9]+)\/([0-9]+) - ([0-9]+):([0-9]+)/', $row, $matches);
-	  		
-	  		$matches [3][0] = '11';
-	  		$matches [4][0] = '2011';
+	  		preg_match_all ('/([a-zA-Z]+), ([0-9]+)\/([0-9]+)\/([0-9]+)( - ([0-9]+):([0-9]+))?/', $row, $matches);
 	  		
 	  		return (object)array (
 	  			'dayName'			=> $matches [1][0],
 	  			'dayOfMonth'	=> $matches [2][0],
 	  			'month'				=> $matches [3][0],
 	  			'year'				=> $matches [4][0],
-	  			'hour'				=> $matches [5][0],
-	  			'minutes'			=> $matches [6][0]
+	  			'hour'				=> $matches [6][0],
+	  			'minutes'			=> $matches [7][0]
 	  		);
 	  	}
   	}
   
-  	$dateStart	= featured_events_get_time ($row ['field_event_date_value']);
-  	$dateEnd		= featured_events_get_time ($row ['field_event_date_value_2']);
+  	$dateStart	= featured_events_get_time (strip_tags ($row ['field_event_date_value']));
+  	$dateEnd		= featured_events_get_time (strip_tags ($row ['field_event_date_value2']));
   ?>
   <script>
     $(function () {
       // Will colorize callendar
-      $('td[date=<?php echo $dateStart -> year; ?>-<?php echo $dateStart -> month; ?>-<?php echo $dateStart -> dayOfMonth; ?>]').addClass ('background-' + '<?php echo $fieldId; ?>');
-      
+      $('td[id=calendar-<?php echo $dateStart -> year; ?>-<?php echo $dateStart -> month; ?>-<?php echo $dateStart -> dayOfMonth; ?>]').addClass ('background-m' + '<?php echo $fieldId; ?>');
+      $('td[date=<?php echo $dateStart -> year; ?>-<?php echo $dateStart -> month; ?>-<?php echo $dateStart -> dayOfMonth; ?>]').addClass ('background-' + '<?php echo $fieldId; ?>')
     });
   </script>
 	<div class="featured-events-row background-<?php echo $fieldId; ?>">
@@ -62,36 +59,73 @@
             	<?php echo ucwords ($row ['city']); ?>, <?php echo $row ['country']; ?>
             </div>
             <div class="description">
-          		<?php echo substr ($row ['body'], 0, 500); ?>  
-	            <?php echo (strlen ($row ['body']) > 500) ? '...' : ''; ?>
+          		<?php echo substr ($row ['body'], 0, 200); ?>  
+	            <?php echo (strlen ($row ['body']) > 200) ? ('... ' . $row ['view_node']) : ''; ?>
             </div>
 					</td>
 					<td class="right-column">
             <div class="user">
-              Username
+              <?php echo $row ['name']; ?>
             </div>
             <div class="details">
               <table>
                 <tbody>
                   <tr>
                     <td class="datetime">
-                      <div class="time">
-                        09:00 - 11:00
-                      </div>
-                      <div class="date">
-                        21/12/2011
-                      </div>
+											<?php if ($dateStart -> year == $dateEnd -> year && $dateStart -> month == $dateEnd -> month && $dateStart -> dayOfMonth == $dateEnd -> dayOfMonth): ?>
+                      	<?php if ($dateStart -> hour !== ''): ?>
+	                        <div class="time">
+	                        	<?php echo $dateStart -> hour; ?>:<?php echo $dateStart -> minutes; ?>
+	                          <?php if ($dateEnd -> hour !== ''): ?>
+	                            - <?php echo $dateEnd -> hour; ?>:<?php echo $dateEnd -> minutes; ?>
+	                          <?php endif; ?>
+	                        </div>
+                        <?php endif; ?>
+                        <div class="date">
+                        	<?php echo $dateStart -> dayOfMonth; ?>/<?php echo $dateStart -> month; ?>/<?php echo $dateStart -> year; ?>
+                        </div>
+                      <?php else: ?>
+                      	<table class="multidate">
+                          <tbody>
+                          	<tr>
+                          		<td>
+                                <div class="date">
+                              		<?php echo $dateStart -> dayOfMonth; ?>/<?php echo $dateStart -> month; ?>/<?php echo $dateStart -> year; ?>,
+                                </div>
+                          		</td>
+                              <td>
+                                <div class="time">
+                              		<?php echo $dateStart -> hour; ?>:<?php echo $dateStart -> minutes; ?> -
+                                </div>
+                              </td>
+                          	</tr>
+                          	<tr>
+                          		<td>
+                                <div class="date">
+                              		<?php echo $dateEnd -> dayOfMonth; ?>/<?php echo $dateEnd -> month; ?>/<?php echo $dateEnd -> year; ?>,
+                                </div>
+                          		</td>
+                              <td>
+                                <div class="time">
+                              		<?php echo $dateEnd -> hour; ?>:<?php echo $dateEnd -> minutes; ?>
+                                </div>
+                              </td>
+                          	</tr>
+                          </tbody>
+                        </table>
+                          	 
+                    	<?php endif; ?>
                     </td>
                     <td class="reservations">
                       <div>
-                        <span class="available">9</span> Available
+                        <span class="available">0</span> Available
                       </div>
                       <div>
-                        <span class="occupied">9</span> Occupied
+                        <span class="occupied">0</span> Occupied
                       </div>
                     </td>
                     <td class="price">
-                      <span class="currency">£</span><span class="amount">5.00</span> 
+                      <span class="currency">£</span><span class="amount">0.00</span> 
                     </td>
                   </tr>
                 </tbody>
