ac295473550b87045d8a744f9970e98eae7e80a8
akierzkowski <adrian.kierzkowski@gmail.com>
2011-12-01 13:38:42 +0000

    #30 Chat integration
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@334 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/themes/saw/node.tpl.php
+++ sites/all/themes/saw/node.tpl.php
@@ -7,7 +7,9 @@
     <h1 class="title"><a href="<?php print $node_url ?>"><?php print $title ?></a></h1>
   <?php endif; ?>
     <span class="submitted"><?php print $submitted ?></span>
-    <div class="taxonomy"><?php print $terms ?></div>
+		<?php if ($terms): ?>
+			<div class="taxonomy"><?php print $terms ?></div>
+		<?php endif; ?>
     <div class="content"><?php print $content ?></div>
     <?php if ($links): ?>
     <div class="links">&raquo; <?php print $links ?></div>
