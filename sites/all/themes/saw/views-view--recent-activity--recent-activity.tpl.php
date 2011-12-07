633d81095733f2f61c2b53a971d0e9fabd3f7de2
akierzkowski <adrian.kierzkowski@gmail.com>
2011-12-07 19:42:10 +0000

    #319 User Profile: new design
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@374 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/themes/saw/views-view--recent-activity--recent-activity.tpl.php
+++ sites/all/themes/saw/views-view--recent-activity--recent-activity.tpl.php
@@ -1,22 +1,28 @@
 	<div class="recent-activity-rows">
 
 
-	<?php foreach ($view -> style_plugin -> rendered_fields as $fieldId => $row): ?>
-	
-		<div class="recent-activity-row">
-	
-			<div class="time-elapsed">
-				<?php echo $row ['created']; ?>
-			</div>
-
-			<div class="message">
-				<?php if (!empty ($row ['name'])) echo $row ['name']; ?>
-				<?php echo $row ['message']; ?>
-			</div>
-				
-		</div>
+	<?php if ($view -> style_plugin -> rendered_fields): ?>
+		<?php foreach ($view -> style_plugin -> rendered_fields as $fieldId => $row): ?>
 		
+			<div class="recent-activity-row">
+		
+				<div class="time-elapsed">
+					<?php echo $row ['created']; ?>
+				</div>
+
+				<div class="message">
+					<?php if (!empty ($row ['name'])) echo $row ['name']; ?>
+					<?php echo $row ['message']; ?>
+				</div>
+					
+			</div>
+			
+		
+		<?php endforeach; ?>
+	<?php else: ?>
 	
-	<?php endforeach; ?>
+		<p><i>No activity yet.</i></p>
+	
+	<?php endif; ?>
 	
 </div>
