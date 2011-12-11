20a0eab3a61ce6a54f5a74693c4f791436e0b824
akierzkowski <adrian.kierzkowski@gmail.com>
2011-12-11 14:20:47 +0000

    #389 Bug: Better theming on forum/new-topic
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@413 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/themes/saw/node.tpl.php
+++ sites/all/themes/saw/node.tpl.php
@@ -3,15 +3,20 @@
 ?>
 <div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
   <?php print $picture ?>
+	<div class="details">
+			<div class="submitted"><?php print $submitted ?></div>
+			<?php if ($terms): ?>
+				<div class="taxonomy"><?php print $terms ?></div>
+			<?php endif; ?>
+	</div>
+
   <?php if ($page == 0): ?>
     <h1 class="title"><a href="<?php print $node_url ?>"><?php print $title ?></a></h1>
   <?php endif; ?>
-    <span class="submitted"><?php print $submitted ?></span>
-		<?php if ($terms): ?>
-			<div class="taxonomy"><?php print $terms ?></div>
-		<?php endif; ?>
-    <div class="content"><?php print $content ?></div>
-    <?php if ($links): ?>
-    <div class="links">&raquo; <?php print $links ?></div>
-    <?php endif; ?>
+	<div class="content"><?php print $content ?></div>
+		<?php if ($links): ?>
+		<div class="links"><?php print $links ?>
+			<div style="clear: both"></div>
+		</div>
+	<?php endif; ?>
 </div>
