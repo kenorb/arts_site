865f6b79ab8e14fd3cbfaf9e1a9d7020e69d9815
akierzkowski <adrian.kierzkowski@gmail.com>
2012-01-28 04:13:28 +0000

    #523 Remove from chat
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@576 2260b2a0-9455-4d21-9559-270ed330a898

---
+++ sites/all/themes/saw/node-chat.tpl.php
@@ -0,0 +1,7 @@
+<div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
+
+	<div class="separator"></div>
+
+	<?php print $content; ?> 
+
+</div>
