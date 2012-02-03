63400858d0b8f6ca33b2114b8d0bc50fa3115927
akierzkowski <adrian.kierzkowski@gmail.com>
2012-02-03 03:18:42 +0000

    #571 Flag as offensive + Flag comment + Moderator Unflag dont work properly Updated custom patch which adds the "mark as spam" permission to allow all users to mark content as spam
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_arts/trunk@633 2260b2a0-9455-4d21-9559-270ed330a898

--- sites/all/themes/saw/comment.tpl.php
+++ sites/all/themes/saw/comment.tpl.php
@@ -1,9 +1,14 @@
 <?php
 // $Id: comment.tpl.php,v 1.6 2008/01/04 19:24:24 goba Exp $
 ?>
-<div class="comment<?php print ' '. $status; ?>">
+<div class="comment<?php print ' '. $status; ?> <?php if ($comment -> is_spam): ?>comment-is-spam<?php endif; ?> ">
 
-	<table>
+	<?php if ($comment -> is_spam): ?>
+		<div class="comment-spam-information">
+			<?php echo t('This comment has been marked as spam') . '.'; ?>
+		</div>
+	<?php endif; ?>
+	<table class="<?php if ($comment -> is_spam): ?>comment-is-spam<?php endif; ?>">
 		<tr>
 			<td class="image-area">
 				<div class="image">
@@ -32,7 +37,15 @@
 				<span class="clinks">
 					<a href="/comment/edit/<?php echo $comment -> cid; ?>" title="Edit comment"><img src="/sites/all/themes/saw/images/comment_edit.png" alt="Edit" /></a>
 					<a href="/comment/delete/<?php echo $comment -> cid; ?>" title="Delete comment"><img src="/sites/all/themes/saw/images/trash-can-delete.png" alt="Delete" /></a>
-					<a href="/spam/comment/<?php echo $comment -> cid; ?>/spam" title="Mark as spam"><img src="/sites/all/themes/saw/images/flag_red.png" alt="Mark as spam" /></a>
+					<?php if ($comment -> is_spam): ?>
+						<?php if (user_access ('administer spam')): ?>
+							<a href="/spam/comment/<?php echo $comment -> cid; ?>/not_spam?token=<?php echo drupal_get_token ('not spam comment ' . $comment -> cid); ?>" title="Mark as not a spam"><img src="/sites/all/themes/saw/images/flag_green.png" alt="Mark as not a spam" /></a>
+						<?php else: ?>
+							<span title="Marked as spam" class="comment-is-spam"><img src="/sites/all/themes/saw/images/flag_red.png" alt="Marked as spam" /></span>
+						<?php endif; ?>
+					<?php else: ?>
+						<a href="/spam/comment/<?php echo $comment -> cid; ?>/spam?token=<?php echo drupal_get_token ('spam comment ' . $comment -> cid); ?>" title="Mark as spam"><img src="/sites/all/themes/saw/images/flag_red.png" alt="Mark as spam" /></a>
+					<?php endif; ?>
 				</span>
 			</td>
 		</tr>
--- sites/all/themes/saw/node.tpl.php
+++ sites/all/themes/saw/node.tpl.php
@@ -1,7 +1,8 @@
 <?php
 // $Id: node.tpl.php,v 1.4 2007/08/07 08:39:36 goba Exp $
 ?>
-<div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?> type-<?php echo arg (0); ?>">
+
+<div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?> type-<?php echo arg (0); ?> typename-<?php echo $node -> type ?>">
   <?php print $picture ?>
 	<?php if ($page == 0): ?>
     <h1 class="title"><a href="<?php print $node_url ?>"><?php print $title ?></a></h1>
